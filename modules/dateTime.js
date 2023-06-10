cae8427cbc6bb85071e1d11c5bd5384c936ee138
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-06-10 12:11:51 -0600

    Add current time

---
+++ modules/dateTime.js
@@ -0,0 +1,8 @@
+import { DateTime } from './luxon.js';
+
+const displayCurrentTime = () => {
+  const now = DateTime.now();
+  return now.toLocaleString(DateTime.DATETIME_MED);
+};
+
+export default displayCurrentTime;
