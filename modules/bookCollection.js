c09265f18da34fc2f3e37e46ac84949e6c662f3c
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-06-12 13:36:14 -0600

    Fix linters

--- modules/bookCollection.js
+++ modules/bookCollection.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+
 import Book from './book.js';
 
 class BookCollection {
--- modules/dateTime.js
+++ modules/dateTime.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+
 import { DateTime } from './luxon.js';
 
 const displayCurrentTime = () => {
